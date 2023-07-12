d14f43e36536995dcf325893378f497403a3779d
Shuvadipta Das <shuvadiptadas8820@gmail.com>
2023-07-12 15:12:59 +0000

    Update 3 files

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,6 +7,10 @@ import darkLogo from "../../assets/logos/dark_logo.png";
 const Footer = () => {
   let year = new Date().getFullYear();
 
+const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+  
   const footerItems = [
     [
       { itemName: "About Us", itemLinks: "/about" },
@@ -54,7 +58,7 @@ const Footer = () => {
         <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
           <div className="footer_body">
             <div className=" footer_main mb-6 md:mb-0">
-              <Link to="/" className="logo_img">
+              <Link to="/" onClick={scrollToTop} className="logo_img">
                 <img
                   src={darkLogo}
                   className=" h-12 w-17 ml-1 mb-2.5  hover:shadow "
@@ -81,10 +85,10 @@ const Footer = () => {
               . All Rights Reserved.
             </span>
             <div className="flex mt-0 space-x-6 sm:justify-center sm:mt-0 mr-14 2xl:mr-0">
-              <Link to="/develop">
+              <Link to="/develop" onClick={scrollToTop}>
                 <BsTwitter className="w-6 h-6 transition-all duration-200 ease-in-out transform hover:scale-110" />
               </Link>
-              <Link to="https://github.com/rohansx/informatician">
+              <Link to="https://github.com/rohansx/informatician" target="_blank">
                 <BsGithub className="w-6 h-6 transition-all duration-200 ease-in-out transform hover:scale-110" />
               </Link>
               <Link to="#">
